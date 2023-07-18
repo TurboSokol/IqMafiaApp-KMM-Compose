8348f2dc162cd903a050af339ca6e1f1fa86f2fb
postal398 <postal398@ya.ru>
2023-07-18 21:27:45 +0300

    after tuesday meet

--- shared-ui/src/commonMain/kotlin/com.turbosokol.iqmafiaapp/screens/CardsScreenView.kt
+++ shared-ui/src/commonMain/kotlin/com.turbosokol.iqmafiaapp/screens/CardsScreenView.kt
@@ -2,7 +2,6 @@ package com.turbosokol.iqmafiaapp.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,17 +13,14 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
-import com.turbosokol.iqmafiaapp.components.IQAlertDialogView
-import com.turbosokol.iqmafiaapp.components.IQCollapsedSwitchFABView
 import com.turbosokol.iqmafiaapp.features.app.AppState
+import com.turbosokol.iqmafiaapp.features.judge.screens.cards.JudgeCardsScreenAction
 import com.turbosokol.iqmafiaapp.features.judge.screens.cards.JudgeCardsScreenState
 import com.turbosokol.iqmafiaapp.features.judge.screens.slots.JudgeSlotsScreenAction
-import com.turbosokol.iqmafiaapp.features.judge.screens.slots.JudgeSlotsScreenState
 import com.turbosokol.iqmafiaapp.theme.Colors
 import com.turbosokol.iqmafiaapp.theme.Dimensions
 import com.turbosokol.iqmafiaapp.theme.Strings
@@ -43,66 +39,34 @@ import kotlinx.coroutines.flow.StateFlow
 @Composable
 fun CardsScreenView(viewModel: ReduxViewModel) {
     Text(text = "Cards\nCard roles randomizer\nWith choose of card in stack")
-    CardsSingleScreenView(viewModel)
-}
 
-
-//SINGLE - в этом контексте - ОДНА (не турнамент) а НЕ одиночная игра.
-@Composable
-fun CardsSingleScreenView(viewModel: ReduxViewModel) {
     //StateFlow вроде как для сохранения во вью модели всяких данных
-    val stateFlow: StateFlow<AppState> = viewModel.store.observeState()
+    val stateFlow: StateFlow<AppState> = viewModel.store.observeState()//observable
     //Сам appState содержет кучу состояний, примеры: navigationState, judgeDayScreenState, judgeRoundState
     val appState by stateFlow.collectAsState(Dispatchers.Main)
     //CardsState это уже я добавил
-    val CardsState: JudgeCardsScreenState = appState.getJudgeCardsState() //тут смены нэйминга со Slots на Cards
+    val сardsState: JudgeCardsScreenState = appState.getJudgeCardsState() //тут смены нэйминга со Slots на Cards
 
     Column(modifier = Modifier.fillMaxSize()) {
-        if (CardsState.listIndex >= 0) {
-            val showingCards = CardsState.cardsList.subList(0, CardsState.listIndex)
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Colors.orange.copy(alpha = 0.1f))
-                    .padding(start = Dimensions.Padding.medium, end = Dimensions.Padding.medium)
-                )
-                    {
-                        showingCards.forEach { item ->
-                            Box(
-                                modifier = Modifier
-                                    .padding(Dimensions.Padding.xmedium)
-                                    .background(color = Colors.imageBackground)
-                            ) {
-                                Text(
-                                    text = item.toString(),
-                                    fontSize = Dimensions.TextSize.large,
-                                    color = Colors.primary,
-                                    textDecoration = TextDecoration.LineThrough,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
-                        }
-            }//Конец Row
-        }
 
         TextButton(modifier = Modifier.fillMaxSize()
             .background(color = Colors.orange.copy(alpha = 0.1f))
             .pointerInput(Unit) {
                 detectTapGestures(onLongPress = {
                     viewModel.execute(
-                        JudgeSlotsScreenAction.SetResetDialogVisible
+                        JudgeCardsScreenAction.ShowNext
                     )
                 })
             }, onClick = {
-            if (CardsState.cardsList.lastIndex != CardsState.listIndex) {
-                viewModel.execute(JudgeSlotsScreenAction.ShowNext)
+            if (сardsState.cardsList.lastIndex != сardsState.listIndex) {
+                viewModel.execute(JudgeCardsScreenAction.ShowNext)
             } else {
-                viewModel.execute(JudgeSlotsScreenAction.SetResetDialogVisible)
+                viewModel.execute(JudgeCardsScreenAction.ShowNext)
             }
         }) {
             Text(
-                text = if (CardsState.isHidden) Strings.singleSlotsHiddenLabel else CardsState.cardsList[CardsState.listIndex].toString(),
-                fontSize = if (CardsState.isHidden) Dimensions.TextSize.huge else Dimensions.TextSize.xhuge,
+                text = if (сardsState.isHidden) Strings.singleSlotsHiddenLabel else сardsState.cardsList[сardsState.listIndex].toString(),
+                fontSize = if (сardsState.isHidden) Dimensions.TextSize.huge else Dimensions.TextSize.xhuge,
                 color = Colors.primary
             )
         }
