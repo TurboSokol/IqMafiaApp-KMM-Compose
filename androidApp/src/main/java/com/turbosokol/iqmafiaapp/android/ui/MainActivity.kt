0911cd7e329b0bc189d9f24e71b225b561c31d3c
Vadim <maslovvadim398@gmail.com>
2023-08-24 14:19:04 +0300

    disable native android navigation

--- androidApp/src/main/java/com/turbosokol/iqmafiaapp/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/turbosokol/iqmafiaapp/android/ui/MainActivity.kt
@@ -3,17 +3,6 @@ package com.turbosokol.iqmafiaapp.android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.turbosokol.iqmafiaapp.common.BottomNavBarView
 import com.turbosokol.iqmafiaapp.viewmodel.ReduxViewModel
 import org.koin.android.ext.android.inject
@@ -25,26 +14,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Navigation()
+//            Navigation()
             BottomNavBarView(viewModel = viewModel)
         }
     }
-
-    @Composable
-    fun Navigation() {
-        val navController = rememberNavController()
-        NavHost(navController = navController,
-            startDestination = "splash_screen") {
-            composable("splash_screen") {
-                IQSplashScreen(navController = navController)
-            }
-
-            // Main Screen
-            composable("main_screen") {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(text = "Main Screen", color = Color.Black, fontSize = 24.sp)
-                }
-            }
-        }
-    }
 }
+
+//    @Composable
+//    fun Navigation() {
+//        val navController = rememberNavController()
+//        NavHost(navController = navController,
+//            startDestination = "splash_screen") {
+//            composable("splash_screen") {
+//                IQSplashScreen(navController = navController)
+//            }
+//
+//            // Main Screen
+//            composable("main_screen") {
+//                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+//                    Text(text = "Main Screen", color = Color.Black, fontSize = 24.sp)
+//                }
+//            }
+//        }
+//    }
+//}
