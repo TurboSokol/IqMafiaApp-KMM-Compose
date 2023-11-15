e2e014aa4155202388d8301bbf660cc067494172
Vadim <maslovvadim398@gmail.com>
2023-11-15 14:31:27 +0300

    dark colors scheme added

--- shared-ui/src/commonMain/kotlin/com.turbosokol.iqmafiaapp/theme/Colors.kt
+++ shared-ui/src/commonMain/kotlin/com.turbosokol.iqmafiaapp/theme/Colors.kt
@@ -21,5 +21,16 @@ object Colors {
     val md_theme_light_background = Color(0x00000000) //транспарент
     val md_theme_light_onBackground = Color(0xFFFFFFFF) //белый
     val md_theme_light_outline = Color(0xFF757575) //серый
+
+    val md_theme_dark_primary = Color(0x302723) //Шиммер!!
+    val md_theme_dark_onPrimary = Color(0xFFFFFFFF) //темно-серый, основной цвет шрифта!!
+    val md_theme_dark_inversePrimary = Color(0xFF81C784/*0x7e387b*/) //зеленый цвет, фон, текст!!
+    val md_theme_dark_primaryContainer =  Color(0xFFEF5350) //голубой цвет, контейнер боттом бара!!
+    val md_theme_dark_secondary = Color(0x1f2bc7) //оранжевый цвет, фон!!
+    val md_theme_dark_secondaryContainer = Color(0x0c0c0c) //светло-серый!!
+    val md_theme_dark_tertiary = Color(0xff2dd7e0)//красный, фон, текст !!
+    val md_theme_dark_background = Color(0x00000000) //транспарент!!
+    val md_theme_dark_onBackground = Color(0xFF202020) //белый !!
+    val md_theme_dark_outline = Color(0x8a8a8a) //серый!!
 }
 
